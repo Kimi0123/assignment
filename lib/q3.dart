6c2f9cd09850ddd2a98ac72c36861bbe95e5f252
Kimi0123 <maharjannirvan01@gmail.com>
2026-04-04 20:56:21 +0545

    q3

--- lib/q3.dart
+++ lib/q3.dart
@@ -1,5 +1,19 @@
 import "dart:io";
 
-void main(){
-  
+void main() {
+  print("Enter the number of classes held:");
+  int held = int.parse(stdin.readLineSync()!);
+
+  print("Enter the number of classes attended:");
+  int attend = int.parse(stdin.readLineSync()!);
+
+  double percentage = (attend / held) * 100;
+
+  print("your percentage is $percentage");
+
+  if (percentage >= 75) {
+    print("You are eligible to attend the exam");
+  } else {
+    print("you are not eligible to attend the exam");
+  }
 }
